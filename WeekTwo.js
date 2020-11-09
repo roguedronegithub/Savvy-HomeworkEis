a109a71f2599e8fbf18f8616f3c22924f50fed8a
Brian <2019roguedrone@gmail.com>
2020-11-09 13:46:37 -0600

    homework

---
+++ WeekTwo.js
@@ -0,0 +1,43 @@
+Let pizzaPlace = "Eis's Place";
+  //variable  for homework
+
+  Let NumberOfToppings = 10;
+ //Variable for homework
+
+ console.log (NumberOfToppings);
+ console.log (pizzaPlace);
+ // printing variables to check output
+
+ console.log ('Head to ${pizzaPlace} for the best pies in town with over ${NumberOfToppings} toppings to choose from for only five dollars');
+  //I created a sentence using template literal expressions
+
+  if (toppings <10)
+    offering = "Quality, not quantity.";
+
+if (toppings >10)
+    offering = "A whole lot of pizza.";
+ // I created two if statements with one having less then 10 and greater then 10
+
+
+ //Bonus create a for loop
+
+ let = i;
+    for ( i = 0; i < 10; i++ ) {
+      text += "the number is" + "NumberOfToppings";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
